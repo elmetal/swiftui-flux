dd9766a96fe45688a76f3a61846c227640bea31e
elmetal <nm7.pupu@gmail.com>
2019-08-06 18:09:48 +0900

    init swiftui proj

---
+++ swiftui-flux/swiftui-flux/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  swiftui-flux
+//
+//  Created by elmetal on 2019/08/06.
+//  Copyright © 2019 s001033. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
